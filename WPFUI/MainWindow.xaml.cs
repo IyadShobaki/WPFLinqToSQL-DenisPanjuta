b1fbf1a8f4f6a8949c576e34a494ec80e3f6dbd0
IyadShobaki <iyadshobaki82@gmail.com>
2020-08-05 16:34:49 -0400

    Inserting Objects into the Database
    insert universtiies and students information using data context.

--- WPFUI/MainWindow.xaml.cs
+++ WPFUI/MainWindow.xaml.cs
@@ -28,10 +28,52 @@ namespace WPFUI
             InitializeComponent();
 
             string connectionString = ConfigurationManager
-                .ConnectionStrings["LinqToSQL-DenisPanjuta.Properties.Settings.PanjutorialsDBConnectionString"].ConnectionString;
+                .ConnectionStrings["WPFUI.Properties.Settings.PanjutorialsDBConnectionString"].ConnectionString;
 
             dataContext = new LinqToSqlDataClassesDataContext(connectionString);
 
+            //InsertUniversities();
+
+            InsertStudents();
+        }
+
+        public void InsertUniversities()
+        {
+            dataContext.ExecuteCommand("delete from University");
+
+            University yale = new University();
+            yale.Name = "Yale";
+            dataContext.Universities.InsertOnSubmit(yale);
+
+            University beijingTech = new University();
+            beijingTech.Name = "Beijing Tech";
+            dataContext.Universities.InsertOnSubmit(beijingTech);
+
+            dataContext.SubmitChanges();
+
+            MainDataGrid.ItemsSource = dataContext.Universities;
+        }
+        public void InsertStudents()
+        {
+
+            University yale = dataContext.Universities.First(
+                un => un.Name.Equals("Yale"));
+            University beijingTech = dataContext.Universities.First(
+                un => un.Name.Equals("Beijing Tech"));
+
+            List<Student> students = new List<Student>();
+
+            students.Add(new Student { Name = "Carla", Gender = "female", UniversityId = yale.Id });
+            students.Add(new Student { Name = "Toni", Gender = "male", University = yale });
+            students.Add(new Student { Name = "Leyla", Gender = "female", University = beijingTech });
+            students.Add(new Student { Name = "James", Gender = "trans-gender", University = beijingTech });
+
+
+            dataContext.Students.InsertAllOnSubmit(students);
+
+            dataContext.SubmitChanges();
+
+            MainDataGrid.ItemsSource = dataContext.Students;
         }
     }
 }
